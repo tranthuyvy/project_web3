6b7e061c3c6cee891709970797cb491445432e5f
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-19 23:47:12 +0700

    .

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCounter;
+
+    event Transfer(
+        address from,
+        address receiver,
+        uint amout,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+}
