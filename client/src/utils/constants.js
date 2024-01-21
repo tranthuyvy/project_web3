af1bfdb8550e8fc27215a0d785bcbba1cd489038
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-21 13:42:04 +0700

    constant

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+export const contractAddress = '0x0F67ccF8183C1085fB5a6a9bddFAB93d731d7CDB'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/pI5Tau9PpeiG5ik-29L7SzMfCRAXLQ-N
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/pI5Tau9PpeiG5ik-29L7SzMfCRAXLQ-N',
+      accounts: ['5501d2efa11b033c7c444074e4b0179001bb6fb6d5eb2e4220215e4a2c080518']
+    }
+  }
+}
