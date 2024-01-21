bab51af2d1b22e1d29917a714965a770b51bb6d9
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-01-21 22:20:26 +0700

    Virtual Transactions

---
+++ client/src/utils/data.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 1,
+    url: 'https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284',
+    message: '',
+    timestamp: '01/01/2024, 4:33:21 PM',
+    addressFrom: '0xF81a55A3ab9e55FC49E961E3FA8C3658Ba558959',
+    amount: '0.01',
+    addressTo: '0x07b3614e3Ba381Ca03475bdBca0dBB1422BAC347'
+  },
+  {
+    id: 2,
+    url: 'https://media4.popsugar-assets.com/files/2013/11/07/832/n/1922398/eb7a69a76543358d_28.gif',
+    message: '',
+    timestamp: '02/01/2024, 4:33:21 PM',
+    addressFrom: '0x07b3614e3Ba381Ca03475bdBca0dBB1422BAC347',
+    amount: '0.01',
+    addressTo: '0xF81a55A3ab9e55FC49E961E3FA8C3658Ba558959'
+  },
+  {
+    id: 3,
+    url: 'https://acegif.com/wp-content/uploads/gif-shaking-head-38.gif',
+    message: '',
+    timestamp: '04/04/2023, 4:33:21 PM',
+    addressFrom: '0x07b3614e3Ba381Ca03475bdBca0dBB1422BAC347',
+    amount: '0.01',
+    addressTo: '0xF81a55A3ab9e55FC49E961E3FA8C3658Ba558959'
+  },
+  {
+    id: 4,
+    url: 'https://i.pinimg.com/originals/68/a0/9e/68a09e774e98242871c2db0f99307420.gif',
+    message: '',
+    timestamp: '01/05/2023, 4:33:21 PM',
+    addressFrom: '0x07b3614e3Ba381Ca03475bdBca0dBB1422BAC347',
+    amount: '0.01',
+    addressTo: '0xF81a55A3ab9e55FC49E961E3FA8C3658Ba558959'
+  },
+  {
+    id: 5,
+    url: 'https://i.pinimg.com/originals/73/d3/a1/73d3a14d212314ab1f7268b71d639c15.gif',
+    message: '',
+    timestamp: '20/11/2023, 4:33:21 PM',
+    addressFrom: '0x07b3614e3Ba381Ca03475bdBca0dBB1422BAC347',
+    amount: '0.01',
+    addressTo: '0xF81a55A3ab9e55FC49E961E3FA8C3658Ba558959'
+  },
+  {
+    id: 6,
+    url: 'https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif',
+    message: '',
+    timestamp: '21/01/2024, 4:33:21 PM',
+    addressFrom: '0x07b3614e3Ba381Ca03475bdBca0dBB1422BAC347',
+    amount: '0.01',
+    addressTo: '0xF81a55A3ab9e55FC49E961E3FA8C3658Ba558959'
+  }
+]
